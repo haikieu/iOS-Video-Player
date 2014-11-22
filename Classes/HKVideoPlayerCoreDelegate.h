66a6b5085c88f62f17e28ce21d41a323f5dfd51a
Hai Kieu <haikieu2907@gmail.com>
2014-11-22 14:55:35 +0700

    Implement  HKVideoPlayerCoreDelegate

--- Classes/HKVideoPlayerCoreDelegate.h
+++ Classes/HKVideoPlayerCoreDelegate.h
@@ -14,14 +14,32 @@
 -(void) playerDidLoad;
 -(void) playerDidFailure;
 -(void) playerDidReady;
+
+-(void) playerWillPlay;
 -(void) playerDidPlay;
+
+-(void) playerWillStop;
 -(void) playerDidStop;
+
+-(void) playerWillPause;
 -(void) playerDidPause;
+
+-(void) playerWillEnterFullscreen;
 -(void) playerDidEnterFullscreen;
+
+-(void) playerWillExitFullscreen;
 -(void) playerDidExitFullscreen;
+
+-(void) playerWillRewind:(float)speed;
 -(void) playerDidRewind:(float)speed;
--(void) playerDidForward:(float)speed;
+
+-(void) playerWillFastforward:(float)speed;
+-(void) playerDidFastforward:(float)speed;
+
+-(void) playerWillUpdatePosition:(float)position;
 -(void) playerDidUpdatePosition:(float)position;
+
+-(void) playerWillCloseView;
 -(void) playerDidCloseView;
 
 @end
