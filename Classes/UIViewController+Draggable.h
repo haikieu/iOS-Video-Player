b40f9853a9d788ec80f4581c98a1e095e034a52e
Hai Kieu <haikieu2907@gmail.com>
2014-11-25 19:34:58 +0700

    Add new files and bundles

---
+++ Classes/UIViewController+Draggable.h
@@ -0,0 +1,41 @@
+//
+//  UIViewController+Draggable.h
+//  iOS Video Player
+//
+//  Created by Hai Kieu on 11/25/14.
+//  Copyright (c) 2014 haikieu2907@gmail.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIViewController (Draggable)
+
+/**-----------------------------------------------------------------------------
+ * @name UIView+draggable Properties
+ * -----------------------------------------------------------------------------
+ */
+
+/** The pan gestures that handles the view dragging
+ *
+ * @param panGesture The tint color of the blurred view. Set to nil to reset.
+ */
+@property (nonatomic) UIPanGestureRecognizer *panGesture;
+
+/**-----------------------------------------------------------------------------
+ * @name UIView+draggable Methods
+ * -----------------------------------------------------------------------------
+ */
+
+/** Enables the dragging
+ *
+ * Enables the dragging state of the view
+ */
+- (void)enableDragging;
+
+/** Disable or enable the view dragging
+ *
+ * @param draggable The boolean that enables or disables the draggable state
+ */
+- (void)setDraggable:(BOOL)draggable;
+
+@end
