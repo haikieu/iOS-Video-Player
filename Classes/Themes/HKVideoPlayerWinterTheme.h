4069e7a724bcb8a3492be827d3dba0dd74f78837
Hai Kieu <haikieu2907@gmail.com>
2014-11-27 13:58:33 +0700

    Add more theme classes

---
+++ Classes/Themes/HKVideoPlayerWinterTheme.h
@@ -0,0 +1,13 @@
+//
+//  HKVideoPlayerWinterTheme.h
+//  iOS Video Player
+//
+//  Created by HK on 11/27/14.
+//  Copyright (c) 2014 haikieu2907@gmail.com. All rights reserved.
+//
+
+#import "HKVideoPlayerThemeView.h"
+
+@interface HKVideoPlayerWinterTheme : HKVideoPlayerThemeView
+
+@end
---
+++ Classes/Themes/HKVideoPlayerWoodenTheme.m
@@ -0,0 +1,13 @@
+//
+//  HKVideoPlayerWoodenTheme.m
+//  iOS Video Player
+//
+//  Created by HK on 11/27/14.
+//  Copyright (c) 2014 haikieu2907@gmail.com. All rights reserved.
+//
+
+#import "HKVideoPlayerWoodenTheme.h"
+
+@implementation HKVideoPlayerWoodenTheme
+
+@end
