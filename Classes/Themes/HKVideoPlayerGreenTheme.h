c658bc45b0f285cc2b71bb380368f4711aee5d80
Hai Kieu <haikieu2907@gmail.com>
2014-11-28 02:10:49 +0700

    Add green themes

---
+++ Classes/Themes/HKVideoPlayerGreenTheme.h
@@ -0,0 +1,13 @@
+//
+//  HKVideoPlayerGreenTheme.h
+//  iOS Video Player
+//
+//  Created by Hai Kieu on 11/28/14.
+//  Copyright (c) 2014 haikieu2907@gmail.com. All rights reserved.
+//
+
+#import "HKVideoPlayerThemeView.h"
+
+@interface HKVideoPlayerGreenTheme : HKVideoPlayerThemeView
+
+@end
---
+++ Classes/Themes/HKVideoPlayerGreenTheme.m
@@ -0,0 +1,31 @@
+//
+//  HKVideoPlayerGreenTheme.m
+//  iOS Video Player
+//
+//  Created by Hai Kieu on 11/28/14.
+//  Copyright (c) 2014 haikieu2907@gmail.com. All rights reserved.
+//
+
+#import "HKVideoPlayerGreenTheme.h"
+
+@implementation HKVideoPlayerGreenTheme
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
