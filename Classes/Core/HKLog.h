448a9ef410d5dfb911e039f553441a7db52a9a52
Hai Kieu <haikieu2907@gmail.com>
2014-11-28 13:09:41 +0700

    Add HKLog files

---
+++ Classes/Core/HKLog.h
@@ -0,0 +1,13 @@
+//
+//  HKLog.h
+//  iOS Video Player
+//
+//  Created by HK on 11/28/14.
+//  Copyright (c) 2014 haikieu2907@gmail.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HKLog : NSObject
+
+@end
---
+++ Classes/Core/HKLog.m
@@ -0,0 +1,13 @@
+//
+//  HKLog.m
+//  iOS Video Player
+//
+//  Created by HK on 11/28/14.
+//  Copyright (c) 2014 haikieu2907@gmail.com. All rights reserved.
+//
+
+#import "HKLog.h"
+
+@implementation HKLog
+
+@end
