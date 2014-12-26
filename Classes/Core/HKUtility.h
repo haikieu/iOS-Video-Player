7dcdb4d3ac747dc36259c769f376f5fd8c2b9f70
Hai Kieu <haikieu2907@gmail.com>
2014-12-27 00:55:27 +0700

    Add HKUtility

---
+++ Classes/Core/HKUtility.h
@@ -0,0 +1,24 @@
+//
+//  HKUtility.h
+//  iOS Video Player
+//
+//  Created by Hai Kieu on 12/27/14.
+//  Copyright (c) 2014 haikieu2907@gmail.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#define SYSTEM_VERSION [[UIDevice currentDevice] systemVersion]
+#define SYSTEM_VERSION_EQUAL_TO(v)                  ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedSame)
+#define SYSTEM_VERSION_GREATER_THAN(v)              ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedDescending)
+#define SYSTEM_VERSION_GREATER_THAN_OR_EQUAL_TO(v)  ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] != NSOrderedAscending)
+#define SYSTEM_VERSION_LESS_THAN(v)                 ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedAscending)
+#define SYSTEM_VERSION_LESS_THAN_OR_EQUAL_TO(v)     ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] != NSOrderedDescending)
+
+
+@interface HKUtility : NSObject
+
++(NSString*) iOSVersion;
+
+
+@end
---
+++ Classes/Core/HKUtility.m
@@ -0,0 +1,18 @@
+//
+//  HKUtility.m
+//  iOS Video Player
+//
+//  Created by Hai Kieu on 12/27/14.
+//  Copyright (c) 2014 haikieu2907@gmail.com. All rights reserved.
+//
+
+#import "HKUtility.h"
+
+@implementation HKUtility
+
++(NSString *)iOSVersion
+{
+    return SYSTEM_VERSION;
+}
+
+@end
