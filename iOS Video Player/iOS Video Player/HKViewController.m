90ea962e5650f45cb63d7ace403576d788ccb91b
Hai Kieu <haikieu2907@gmail.com>
2014-12-30 01:20:58 +0700

    Implement menu table for demo app

--- iOS Video Player/iOS Video Player/HKViewController.m
+++ iOS Video Player/iOS Video Player/HKViewController.m
@@ -10,9 +10,16 @@
 #import "HKVideoPlayerViewController.h"
 #import "HKVideoPlayerDefaultTheme.h"
 #import "HKVideoPlayerClassicalTheme.h"
-@interface HKViewController ()
+#import "HKUtility.h"
+#import "HKVideoPlayerCoreView.h"
+#import "HKVideoPlayerViewControllerDelegate.h"
+
+
+@interface HKViewController ()<HKVideoPlayerViewControllerDelegate, UITableViewDelegate,UITableViewDataSource>
 
 @property HKVideoPlayerViewController *playerVC;
+
+@property (nonatomic,strong) UITableView * tableView;
 
 @end
 
@@ -28,34 +35,153 @@
 {
     [super viewDidAppear:animated];
     
-    _playerVC = [[HKVideoPlayerViewController alloc] initWithFrame:CGRectMake(20, 20, 600, 400) theme:[HKVideoPlayerClassicalTheme theme]];
-    [_playerVC enableDragging];
-    [_playerVC autoHideThemeView:YES afterTime:3];
-    _playerVC.delegate = self;
+    _tableView = [[UITableView alloc] initWithFrame:self.view.bounds style:UITableViewStylePlain];
+    _tableView.delegate = self;
+    _tableView.dataSource = self;
+    [_tableView setContentInset:UIEdgeInsetsMake(20, 0, 0, 0)];
     
-    if(DEVICE_IS_IPHONE())
+    [self.view addSubview:_tableView];
+    
+
+}
+
+#pragma mark - UITableView datasource
+
+-(NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
+}
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 2;
+}
+
+#pragma mark - UITableView delegate
+
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString * reuseIdentifier = @"reuse_menu_item";
+    UITableViewCell * cell = [tableView dequeueReusableCellWithIdentifier:reuseIdentifier];
+    
+    if(!cell)
     {
-        [self presentViewController:_playerVC animated:YES completion:^{
-            [_playerVC syncMediaToControlCenter:YES];
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:reuseIdentifier];
+    }
+    
+    NSString * className = nil;
+    NSString * title = nil;
+    switch (indexPath.row) {
+        case 0:
+            className = @"HKVideoPlayerDefaultTheme";
+            title = @"Default theme";
+            break;
+            
+        case 1:
+            
+            className = @"HKVideoPlayerClassicalTheme";
+            title = @"Classical theme";
+            break;
+    }
+    
+    cell.detailTextLabel.text = className;
+    cell.textLabel.text = title;
+    
+    return cell;
+}
+
+static UIView * backgroundTheme;
+
+-(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+        UITableViewCell * cell = [tableView cellForRowAtIndexPath:indexPath];
+    
+        Class themeClass = NSClassFromString(cell.detailTextLabel.text);
+    
+        _playerVC = [[[HKVideoPlayerViewController alloc] initWithFrame:CGRectMake(60, 60, 600, 400) theme:[themeClass theme]] goingToAddSubview];
+        [_playerVC enableDragging];
+        [_playerVC autoHideThemeView:YES afterTime:3];
+        _playerVC.delegate = self;
+    
+        if(DEVICE_IS_IPHONE())
+        {
+            [self presentViewController:_playerVC animated:YES completion:^{
+                [_playerVC syncMediaToControlCenter:YES];
+            }];
+    
+        }
+        else
+        {
+            static dispatch_once_t onceToken;
+            dispatch_once(&onceToken, ^{
+                
+                backgroundTheme = [[UIView alloc] initWithFrame:self.view.bounds];
+                backgroundTheme.backgroundColor = [UIColor blackColor];
+                backgroundTheme.alpha = 1;
+            });
+            
+            [self.view addSubview:backgroundTheme];
+            [self.view addSubview:_playerVC.view];
+            
+            _playerVC.view.alpha = 0;
+            backgroundTheme.alpha = 0;
+            [UIView animateWithDuration:1 animations:^{
+                
+                _playerVC.view.alpha = 1;
+                backgroundTheme.alpha = 0.5;
+            } completion:^(BOOL finished) {
+                
+                [_playerVC syncMediaToControlCenter:YES];
+            }];
+        }
+    
+        [_playerVC loadUrl:[NSURL URLWithString:URL_ADVANCED_STREAMING] autoPlay:YES];
+}
+
+
+-(void)videoPlayer:(HKVideoPlayerViewController *)videoPlayer didCloseView:(UIView *)view
+{
+    if(UI_USER_INTERFACE_IDIOM()==UIUserInterfaceIdiomPhone)
+    {
+        [videoPlayer dismissViewControllerAnimated:YES completion:^{
+            //TODO
         }];
-        
     }
     else
     {
-        [self.view addSubview:_playerVC.view];
-        [_playerVC syncMediaToControlCenter:YES];
+        [UIView animateWithDuration:1 animations:^{
+            backgroundTheme.alpha = 0;
+            view.alpha = 0;
+        } completion:^(BOOL finished) {
+            
+            [backgroundTheme removeFromSuperview];
+            
+            [videoPlayer.themeView removeFromSuperview];
+            [videoPlayer.coreView removeFromSuperview];
+            [view removeFromSuperview];
+        }];
+        
     }
-    
-    [_playerVC loadUrl:[NSURL URLWithString:URL_ADVANCED_STREAMING] autoPlay:YES];
 }
+
+#pragma mark - Enhance orientation
+
+-(BOOL)shouldAutorotate
+{
+    return NO;
+    
+}
+
+-(BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)toInterfaceOrientation
+{
+    return NO;
+}
+
 
 -(void)viewWillAppear:(BOOL)animated
 {
     [super viewWillAppear:animated];
-    
-    
-    
-    
 }
 
 
@@ -65,23 +191,5 @@
     // Dispose of any resources that can be recreated.
 }
 
--(void)videoPlayer:(HKVideoPlayerViewController *)videoPlayer didCloseView:(UIView *)view
-{
-    if(UI_USER_INTERFACE_IDIOM()==UIUserInterfaceIdiomPhone)
-    {
-        [videoPlayer dismissViewControllerAnimated:YES completion:^{
-           //TODO
-        }];
-    }
-    else
-    {
-        [UIView animateWithDuration:1 animations:^{
-            view.alpha = 0;
-        } completion:^(BOOL finished) {
-            [view removeFromSuperview];
-        }];
-        
-    }
-}
 
 @end
